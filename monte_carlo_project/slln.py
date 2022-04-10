e2f627026541fe57d5db26d914326d4b86a806c7
Robin Lin <robin.lin99@yahoo.com>
2022-04-09 21:35:27 -0400

    Update slln.py

--- monte_carlo_project/slln.py
+++ monte_carlo_project/slln.py
@@ -1,4 +1,3 @@
-import math
 import random
 
 
@@ -6,10 +5,7 @@ def q(m, n, spike):
     return 1 / (1 + (abs(m - 50)) ** spike + (abs(n - 50)) ** spike)
 
 
-def main():
-    spike = float(input("Enter spikiness value: "))
-    N = int(input("Enter number of iterations: "))
-
+def simulate(spike, N):
     normalization = 0
     for i in range(100):
         for j in range(100):
@@ -23,7 +19,43 @@ def main():
             k + 1
         )
 
-    print("Computed mean = " + str(time_avg))
+    return time_avg
+
+
+def automated_experiment():
+    combinations = [
+        (0.2, 10000),
+        (2, 10000),
+        (11, 10000),
+        (0.2, 50000),
+        (2, 50000),
+        (11, 50000),
+        (0.2, 100000),
+        (2, 100000),
+        (11, 100000),
+    ]
+
+    for combination in combinations:
+        spike = combination[0]
+        N = combination[1]
+        total_for_combination = 0
+        for _ in range(10):
+            total_for_combination += simulate(spike, N)
+
+        print(
+            f"Average E_p(f) for spike = {spike} and N = {N} is: {total_for_combination / 10}"
+        )
+
+
+def manual_experiment():
+    spike = float(input("Enter spikiness value: "))
+    N = int(input("Enter number of iterations: "))
+    time_avg = simulate(spike, N)
+    print(f"Computed mean for spike = {spike} and N = {N} is: " + str(time_avg))
+
+
+def main():
+    automated_experiment()
 
 
 if __name__ == "__main__":
