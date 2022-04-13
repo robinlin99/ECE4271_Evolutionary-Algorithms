762a0fbf1b293a692a7ef68fbd440b68cd5c60b6
Robin Lin <robin.lin99@yahoo.com>
2022-04-13 19:04:40 -0400

    Add Monte Carlo project report

--- monte_carlo_project/code/metropolis_basic_proposal.py
+++ monte_carlo_project/code/metropolis_basic_proposal.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import random
+import statistics
 
 
 def q(m, n, spike):
@@ -12,14 +13,13 @@ def f(m, n):
     return m + n
 
 
-def propose_advanced(curr, step_size):
+def propose_naive(curr):
     m = curr[0]
     n = curr[1]
-
-    # Generate candidate neighbours based on the step size
+    # Generate candidate neighbours
     candidates = []
-    for i in range(-step_size, step_size + 1):
-        for j in range(-step_size, step_size + 1):
+    for i in [-1, 0, 1]:
+        for j in [-1, 0, 1]:
             candidates.append(((m + i) % 100, (n + j) % 100))
 
     return random.choice(candidates)
@@ -93,16 +93,15 @@ def plot_expected_histogram(spike, N):
     plot_3d_histogram(p, f"Expected Histogram with N = {N} and Spikiness = {spike}")
 
 
-def single_experiment():
+def histogram_analysis():
     spike = float(input("Enter spikiness value: "))
     N = int(input("Enter number of iterations: "))
-    step_size = int(input("Enter the step size for the proposal process: "))
 
     # Initialization
     p = [(random.randint(0, 99), random.randint(0, 99))]
 
     for iteration in range(N):
-        p.append(accept_reject(p[-1], propose_advanced(p[-1], step_size), spike))
+        p.append(accept_reject(p[-1], propose_naive(p[-1]), spike))
 
     print("Computed mean = " + str(compute_average(p)))
     plot_3d_histogram(
@@ -112,50 +111,38 @@ def single_experiment():
     plot_expected_histogram(spike, N)
 
 
-def plot_convergence(step_size, spike, N):
-    for plot_id, step in enumerate(step_size):
-        print(f"Plotting plot ID {plot_id}")
-        points = step_size[step]
-        averages = []
-        total_sum = 0
-        for i, point in enumerate(points):
-            total_sum += f(point[0], point[1])
-            averages.append(total_sum / (i + 1))
-        iters = list(range(1, len(points) + 1))
+def statistical_analysis():
+    combinations = [
+        (0.2, 10000),
+        (2, 10000),
+        (11, 10000),
+        (0.2, 50000),
+        (2, 50000),
+        (11, 50000),
+        (0.2, 100000),
+        (2, 100000),
+        (11, 100000),
+    ]
 
-        plt.subplot(len(step_size), 1, plot_id + 1)
-        plt.plot(iters, averages)
-        plt.title(f"Step Size = {step}", fontsize=12)
-        plt.ylim(0, 200)
-    plt.suptitle(
-        f"Convergence of Expected Value at Various Step Sizes at spike = {spike} and N = {N}",
-        fontsize=16,
-    )
-    plt.show()
+    for combination in combinations:
+        spike = combination[0]
+        N = combination[1]
+        hist = []
+        for _ in range(10):
+            # Initialization
+            p = [(random.randint(0, 99), random.randint(0, 99))]
+            for iteration in range(N):
+                p.append(accept_reject(p[-1], propose_naive(p[-1]), spike))
 
+            hist.append(compute_average(p))
 
-def step_size_experiment():
-    spike = float(input("Enter spikiness value: "))
-    N = int(input("Enter number of iterations: "))
-    step_size = dict.fromkeys(list(range(1, 10, 2)))
-
-    for step in step_size:
-        print(f"Processing step size of {step}")
-
-        p = [(random.randint(0, 99), random.randint(0, 99))]
-
-        for iteration in range(N):
-            p.append(accept_reject(p[-1], propose_advanced(p[-1], step), spike))
-
-        step_size[step] = p
-
-        print(f"Computed mean for step_size = {step} is: " + str(compute_average(p)))
-
-    plot_convergence(step_size, spike, N)
+        print(
+            f"Average E_p(f) for spike = {spike} and N = {N} is {statistics.mean(hist)} with standard deviation {statistics.stdev(hist)}"
+        )
 
 
 def main():
-    step_size_experiment()
+    statistical_analysis()
 
 
 if __name__ == "__main__":
---
+++ monte_carlo_project/code/slln.py
@@ -0,0 +1,63 @@
+import random
+import statistics
+
+
+def q(m, n, spike):
+    return 1 / (1 + (abs(m - 50)) ** spike + (abs(n - 50)) ** spike)
+
+
+def simulate(spike, N):
+    normalization = 0
+    for i in range(100):
+        for j in range(100):
+            normalization += q(i, j, spike)
+
+    time_avg = 0
+    for k in range(N):
+        m = random.randint(0, 99)
+        n = random.randint(0, 99)
+        time_avg = (k * time_avg + 10000 * (m + n) * q(m, n, spike) / normalization) / (
+            k + 1
+        )
+
+    return time_avg
+
+
+def automated_experiment():
+    combinations = [
+        (0.2, 10000),
+        (2, 10000),
+        (11, 10000),
+        (0.2, 50000),
+        (2, 50000),
+        (11, 50000),
+        (0.2, 100000),
+        (2, 100000),
+        (11, 100000),
+    ]
+
+    for combination in combinations:
+        spike = combination[0]
+        N = combination[1]
+        hist = []
+        for _ in range(10):
+            hist.append(simulate(spike, N))
+
+        print(
+            f"Average E_p(f) for spike = {spike} and N = {N} is {statistics.mean(hist)} with standard deviation {statistics.stdev(hist)}"
+        )
+
+
+def manual_experiment():
+    spike = float(input("Enter spikiness value: "))
+    N = int(input("Enter number of iterations: "))
+    time_avg = simulate(spike, N)
+    print(f"Computed mean for spike = {spike} and N = {N} is: " + str(time_avg))
+
+
+def main():
+    automated_experiment()
+
+
+if __name__ == "__main__":
+    main()
